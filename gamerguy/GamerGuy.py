d2aed2d65cd3044eb71df2e40ef3d705a04df0fb
moudi757 <mahmoud.chebbani@lau.edu>
2017-07-28 15:43:21 +0300

    Update Version of GamerGuy

--- gamerguy/GamerGuy.py
+++ gamerguy/GamerGuy.py
@@ -4,21 +4,15 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import MySQLdb
 
 
-# SQLAlchemy is about three components which are Table, Mapper and a Class object
-#Declarative is used to define all three instead of having to code everything from scratch
-#Base jere os defined by a Declratibe method to help us with creating tables and mapping them
+#############################################################################################################################################################
+##SECTION 1: DEFINING THE CLASSES THAT REPRESENT THE DATABASE
+#############################################################################################################################################################
+
+#Declarative base is used to create and mapp the classes which are tables in the database
 Base = declarative_base()
-
-#The engine is used to enable us to establish a connection and interaction with the Database 
-#The choice here is MySQL but SQLAlchemy is open to operating with a lot of options out there like SQLlite
-engine = create_engine('mysql+mysqldb://root:@localhost/gamerguy')
-
-
-
-#################################################################
-
 #Class company will represent the table named Company in the Database
 #It has an id and a name which names will mapp accordingly to the database implementation
 #Company has a one to many relationship with table Creator  
@@ -27,30 +21,24 @@ class Company(Base):
 	id = Column(Integer, primary_key=True, unique=True)
 	name = Column(VARCHAR(100))
   
-
-
 #################################################################
-
-
 #Creator is connected to Company through a one to many relationship with Creator being the many side
 #Creator is also connected to Game through a many to many realtionship
-
+# a ForeighKey is required here to establish a connection between table Creator and table Company in the databse
+#Creator is connected to table Company with the relationship method provided by SQLAlchemy library using the attribute backref to creat the relation'creators'
+#meaning: company has many creators
+#Creator is connected to table Game with the relationship method and arrtibute secondary refers to a final table which represents this relationship 
+#and is also built in the databse
 class Creator(Base):
     __tablename__ = 'Creator'
     id = Column(Integer, primary_key=True)
     name = Column(VARCHAR(100))
-    # a ForeighKey is required here to establish a connection between table Creator and table Company in the databse
     company_id = Column(Integer, ForeignKey('Company.id'))
-    #Creator is connected to table Company with the relationship method provided by SQLAlchemy library using the attribute backref to creat the relation'creators'
-    #meaning: company has many creators
     company = relationship(Company, backref=backref('creators', uselist=True))
-    #Creator is connected to table Game with the relationship method and arrtibute secondary refers to a final table which represents this relationship 
-    #and is also built in the databse
     games = relationship('Game', secondary='creator_game')
 		
 
 #################################################################
-
 #Class Game represents the games that are made by creators and is connected to the table Creator through a many to many relationship 
 #Game will also have a declaration of the method relationship with attribute secondary to define the many to many relationship "creator_game" with table Creator
 class Game(Base):
@@ -61,7 +49,6 @@ class Game(Base):
 
 
 #################################################################
-
 #Class CreatorGame that represents the table which is the many to many relationship between Creator and Game is built with a composite primary key 
 #that is made of two foreignkeys each referrs to the primary key in its corresponding table
 class CreatorGame(Base):
@@ -70,23 +57,23 @@ class CreatorGame(Base):
 	game_id = Column(Integer, ForeignKey('Game.id'), primary_key=True)
 
 
-#################################################################
-
-#session is also a part of the sqlalchemy library and as the name suggests, it creates the session that through, the script communicates with the database
-Session = sessionmaker(bind=engine)
-session = Session()
-#The following method creates all the tables in the engine and it is equavilant to CREATE TABLE in raw SQL
-Base.metadata.create_all(engine)
-#bind engine means that it binds the engine to the metadata of Base so the declaraties (Table, Mappers, Classes) can be accessed
-#through an instance of the session
-Base.metadata.bind = engine
 
 
-#################################################################
+##########################################################################################################################################################
+##SECTION 2: DEFINING THE CLASS USED TO INTERACT WITH THE DATABASE - (CREATE DB, INSERT INTO DB, QUERY DB)
+##########################################################################################################################################################
+
 ##This is a helper class that uses the concepts of OOP to make acheiving tasks less repetative
 class callDB():
+
+	def creatDB(self):
+		db1 = MySQLdb.connect(host="localhost",user="root",passwd="")
+		cursor = db1.cursor()
+		sql = 'CREATE DATABASE GamerGuy'
+		cursor.execute(sql)
+
 	# this function will make sure that entered value to the database is a nonexisting new value
-	def callQ(self, x,y):
+	def callQ(self, x,y, session):
 		r = 0
 		while  r == 0:
 			find_Comp_name = session.query(x).filter(x.name==y).first()
@@ -99,7 +86,7 @@ class callDB():
 
 	#this function will add new values to each of the three tables Company, Creator and Game
 	#and they will be mapped together using the logic provided by sqlalchemy library 
-	def addNew(self, x, y, z):
+	def addNew(self, x, y, z, session):
 		newcompany = Company(name=x)
 		newcreator = Creator(name=y)
 		newgame = Game(name=z)
@@ -116,7 +103,7 @@ class callDB():
 
 
 	#this function adds a new game while it has an existing creator mention in the databse
-	def addToExistCreator(self, x, y):
+	def addToExistCreator(self, x, y, session):
 		newgame = Game(name=x)
 		#this query calls the existing creator and uses it as an object to append the new game on
 		creator_Object = session.query(Creator).filter(Creator.name ==y).first()
@@ -129,7 +116,7 @@ class callDB():
 			print('Creator Not found')
 
 	# this function demonstrates some SQLAlchemy queries to show how quering differs from the raw SQL quering 
-	def execQueries(self):
+	def execQueries(self, session):
 
 		print("This is a block of Queries################################")
 		query1 = session.query(Creator).all()
@@ -143,7 +130,7 @@ class callDB():
 
 		print()
 		#value in filter can be changed according to your inputs 
-		query3 = session.query(Game).filter(Game.name=='MetalGear').one()
+		query3 = session.query(Game).filter(Game.name=='Metal Gear Solid').one()
 		print("Game id:", query3.id, " Game name:", query3.name, "Game creator:", query3.creators[0].name)
 		print()
 
@@ -162,46 +149,44 @@ class callDB():
 			print("Creator name: ",x.name, " Game name:", x.games[0].name)
 
 
+#############################################################################################################################################################
+##SECTION 3: DEFINING THE CLASS WHICH HAS THE MAIN METHOD THAT DRIVES THE EXECTION OF THE SCRIPT
+#############################################################################################################################################################
+
+class runningProgram():
+
+		
+		def runProgram(self):
+			calldb = callDB()
+			calldb.creatDB()
+			engine = create_engine('mysql+mysqldb://root:@localhost/gamerguy')
+			Session = sessionmaker(bind=engine)
+			sessionN = Session()
+			#The following method creates all the tables in the engine and it is equavilant to CREATE TABLE in raw SQL
+			Base.metadata.create_all(engine)
+			#bind engine means that it binds the engine to the metadata of Base so the declaraties (Table, Mappers, Classes) can be accessed
+			#through an instance of the session
+			Base.metadata.bind = engine
+			#inserting NEW data into empty database
+			Compname= "Konami"
+			Comp_name = calldb.callQ(Company,Compname, sessionN)
+			Creatorname= "Kojima"
+			Creator_name = calldb.callQ(Creator, Creatorname, sessionN)
+			Gamename= "Metal Gear Solid"
+			Game_name = calldb.callQ(Game, Gamename,sessionN)
+			calldb.addNew(Comp_name,Creator_name,Game_name, sessionN)
+			#inserting a game to an existing creator
+			Game_name2="Death Stranding"
+			Creator_name2="Kojima"
+			calldb.addToExistCreator(Game_name2, Creator_name2,sessionN)
+			#Calling Queries from Database
+			calldb.execQueries(sessionN)
 
 
-#################################################################
+################################################################################################################################################################
+## SECTION 4: RUNNING THE SCRIPT
+################################################################################################################################################################
 
-
-# initiates class callDB()
-calldb = callDB()
-
-# This mechanisim is used to help the user choose either to input new values or add a game to an existing creator
-#A lot more could have been done here like adding creator to Existing Company and so but 
-#to avoid repetation the case of adding Game was most descriptive to show what is required
-print("To add a new Company, Creator and Game press: N")
-print("To add a new Game to an existing Creator press: E")
-pressedVal = input()
-if(pressedVal == 'N'):
-	print("Please Enter Company's Name:")
-	Compname= input()
-	Comp_name = calldb.callQ(Company,Compname)
-
-	print("Please Enter Creator's Name:")
-	Creatorname= input()
-	Creator_name = calldb.callQ(Creator, Creatorname)
-
-	print("Please Enter Games's Name:")
-	Gamename= input()
-	Game_name = calldb.callQ(Game, Gamename)
-
-	calldb.addNew(Comp_name,Creator_name,Game_name)
-
-elif(pressedVal== 'E'):
-	print("Please Enter Games's Name:")
-	Game_name= input()
-	print("Please Enter Creator's Name:")
-	Creator_name= input()
-	calldb.addToExistCreator(Game_name, Creator_name)
-else:
-	print("you pressed something wrong")
-	quit()
-
-
-# calls a bunch of queries to show interaction with the databse
-calldb.execQueries()
+executeProgram = runningProgram()
+executeProgram.runProgram()
 
